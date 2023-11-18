43f6095383c3cca5a88e0a65717ac605b00b9564
theanh20041507 <thehtvn2004@gmail.com>
2023-11-18 22:27:46 +0700

    init project

---
+++ assets/js/login&register.js
@@ -0,0 +1,33 @@
+const container = document.getElementById('container');
+
+const registerBtn = document.getElementById('register');
+
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+
+const signInButton = document.getElementById('sign-in');
+
+signInButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = '/index.html';
+});
+
+const signUpButton = document.getElementById('sign');
+
+signUpButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.location.href = '/index.html';
+});
+
+
+
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const showSearchHistoryBtn = document.getElementById("showSearchHistoryBtn");
 const searchHistory = document.getElementById("searchHistory");
 
 // Thêm sự kiện click vào class btn
-showSearchHistoryBtn.addEventListener("click", function() {
+showSearchHistoryBtn.addEventListener("click", function () {
     // Kiểm tra trạng thái hiển thị của class header__search-history
     if (searchHistory.style.display === "block" || searchHistory.style.display === "") {
         searchHistory.style.display = "none"; // Ẩn class header__search-history
@@ -11,3 +11,63 @@ showSearchHistoryBtn.addEventListener("click", function() {
         searchHistory.style.display = "block"; // Hiển thị class header__search-history
     }
 });
+
+
+// js detail
+function changeMainImage(clickedImage) {
+    var mainImage = document.querySelector('.detail__img--main img');
+    var newImageUrl = clickedImage.src;
+
+    mainImage.src = newImageUrl;
+    mainImage.style.transform = 'scale(1)'; // Reset the scale to 1 when changing the main image
+
+    mainImage.addEventListener('mouseenter', function () {
+        mainImage.style.transform = 'scale(1.2)';
+    });
+
+    mainImage.addEventListener('mouseleave', function () {
+        mainImage.style.transform = 'scale(1)';
+    });
+}
+
+// document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
+    var descriptionItems = document.querySelectorAll('.detail__intro--extra__item');
+
+    descriptionItems.forEach(function (descriptionItem) {
+        var descriptionIcon = descriptionItem.querySelector('.fa-plus');
+        var descriptionContent = descriptionItem.querySelector('.more__description');
+        var additionalInformation = descriptionItem.querySelector('.more__additional--information')
+        var reviewContent = descriptionItem.querySelector('.more__review');
+
+        // Add click event listener to each description item
+        descriptionItem.addEventListener('click', function () {
+            // Toggle the visibility of the description content
+            if (descriptionContent) {
+                descriptionContent.style.display = (descriptionContent.style.display === 'none') ? 'block' : 'none';
+            }
+
+            // Toggle the visibility of additional information
+            if (additionalInformation) {
+                additionalInformation.style.display = (additionalInformation.style.display === 'none') ? 'block' : 'none';
+            }
+            if (reviewContent) {
+                reviewContent.style.display = (reviewContent.style.display === 'none') ? 'block' : 'none';
+            }
+
+            // Toggle the plus and minus icons
+            if (descriptionIcon) {
+                if (descriptionIcon.classList.contains('fa-plus')) {
+                    descriptionIcon.classList.remove('fa-plus');
+                    descriptionIcon.classList.add('fa-minus');
+                } else {
+                    descriptionIcon.classList.remove('fa-minus');
+                    descriptionIcon.classList.add('fa-plus');
+                }
+            }
+        });
+    });
+});
+// star
+
+
